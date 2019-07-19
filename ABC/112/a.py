170ead53d9c63f7ff25335d1a50b49e9c9e789d4
tapioka324 <26566765+tapioka324@users.noreply.github.com>
2019-07-19 21:13:57 +0900

    Solve: A - Programming Education

---
+++ ABC/112/a.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# input & solve
+N = int(input())
+
+if N == 1:
+    ans = 'Hello World'
+if N == 2:
+    A = int(input())
+    B = int(input())
+    ans = A + B
+
+# output
+print(ans)
