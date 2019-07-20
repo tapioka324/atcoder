4a04d05f1432781bcc98a64f735575587fbaee4f
tapioka324 <26566765+tapioka324@users.noreply.github.com>
2019-07-20 16:13:08 +0900

    Solve: A - Double Helix

---
+++ ABC/122/a.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# input
+b = input()
+
+# solve
+if b == 'A':
+    ans = 'T'
+elif b  == 'T':
+    ans = 'A'
+elif b == 'C':
+    ans = 'G'
+else:
+    ans = 'C'
+
+# output
+print(ans)
