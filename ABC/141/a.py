0536b2a62568c3fd082cedb698f95e4e05780f0c
tapioka324 <26566765+tapioka324@users.noreply.github.com>
2019-09-15 22:48:00 +0900

    Solve: A - Weather Prediction

---
+++ ABC/141/a.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# input
+S = input()
+
+# solve
+if S == 'Sunny':
+    ans = 'Cloudy'
+elif S == 'Cloudy':
+    ans = 'Rainy'
+else:
+    ans = 'Sunny'
+
+# output
+print(ans)
