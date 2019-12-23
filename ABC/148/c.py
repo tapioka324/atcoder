59ff5e2db86721c841d8909695af40d2d3a9c612
tapioka324 <26566765+tapioka324@users.noreply.github.com>
2019-12-23 21:37:25 +0900

    AC: C - Snack

---
+++ ABC/148/c.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+A, B = map(int, input().split())
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+
+    return a
+
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+print(lcm(A, B))
