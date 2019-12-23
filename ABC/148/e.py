3831bf93574e175710248fdd3613f928b6ab4ae8
tapioka324 <26566765+tapioka324@users.noreply.github.com>
2019-12-23 22:47:26 +0900

    AC: E - Double Factorial

---
+++ ABC/148/e.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+N = int(input())
+
+def g1(n, p):
+    if n == 0:
+        return 0
+
+    return n // p + g1(n // p, p)
+
+
+def g2(n, p):
+    if n % 2 == 1:
+        return g1(n, p) - g2(n - 1, p)
+    res = g1(n // 2, p)
+    if p == 2:
+        res += n // 2
+    return res
+
+
+ans = min(g2(N, 2), g2(N, 5))
+print(ans)
