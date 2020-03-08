752b6e7f4d4a37a2855d67e95b3f2278e969e5bd
tapioka324 <26566765+tapioka324@users.noreply.github.com>
2020-03-08 21:43:09 +0900

    AC: C - Tax Increase

---
+++ ABC/158/c.py
@@ -0,0 +1,11 @@
+import math
+
+A, B = map(int, input().split())
+
+ans = -1
+for i in range(1, 1001):
+    if math.floor(i * 0.08) == A and math.floor(i * 0.1) == B:
+        ans = i
+        break
+
+print(ans)
